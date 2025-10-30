e565f7fd842f1ac8d0cdcf00691a9291ccd59887
Kert-Ace T. Ombion <155365666+acesuus@users.noreply.github.com>
2025-10-30 22:24:58 +0800

    removed secret key on settings.py

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -6,23 +6,23 @@ UPDATED with recommended configurations for food tracker
 from pathlib import Path
 import os
 from decouple import config
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 CALORIENINJAS_API_KEY = config('CALORIENINJAS_API_KEY')
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wokckwn7#x1pdu(!@*&z1wskt)o))t@6(z9^pb*1r4h1dm_g5&'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+
 
 ALLOWED_HOSTS = []
 
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tracker',  # Your food tracker app
+    'tracker',  
 
 ]
 
