15ba85da7e37046a7634ef1dab1942ab63eeb42e
John Brence S. Condesa <144252928+Bitr3e@users.noreply.github.com>
2025-11-11 01:40:59 +0800

    Update URLs Configuration

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),  # Add this for social auth
     path('', include('tracker.urls')),  # Include tracker app URLs at root]
 ]
